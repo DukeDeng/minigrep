405ab802c49e1237720b5dfdef526207300b5f26
samurai <452146589@qq.com>
2024-07-03 15:50:43 +0800

    chore: test

--- src/lib.rs
+++ src/lib.rs
@@ -22,3 +22,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     println!("With text:\n{contents}");
     Ok(())
 }
+
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
